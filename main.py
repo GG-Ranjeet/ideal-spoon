80c601dcd4e4ddcd38315725259037d6d99f527a
Ranjeet singh <117429838+GG-Ranjeet@users.noreply.github.com>
2026-01-02 09:07:28 +0530

    Update database URI configuration and disable debug mode

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ def load_user(user_id):
 
 class Base(DeclarativeBase):
     pass
-# app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get("DB_URI","sqlite:///posts.db")
-app.config['SQLALCHEMY_DATABASE_URI'] =  "sqlite:///posts.db"
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get("DB_URI","sqlite:///posts.db")
+# app.config['SQLALCHEMY_DATABASE_URI'] =  "sqlite:///posts.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -330,4 +330,4 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
